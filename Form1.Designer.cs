26a2564b73b9a33619770e01c094f7967f2eacf0
Muris.Cengic <muris.cengic@outlook.com>
2024-03-21 11:20:55 +0400

    Display categories

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dgv_Categories = new DataGridView();
+            dgv_Tasks = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgv_Categories).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgv_Tasks).BeginInit();
             SuspendLayout();
+            // 
+            // dgv_Categories
+            // 
+            dgv_Categories.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv_Categories.BackgroundColor = SystemColors.ActiveBorder;
+            dgv_Categories.BorderStyle = BorderStyle.Fixed3D;
+            dgv_Categories.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dgv_Categories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_Categories.ColumnHeadersVisible = false;
+            dgv_Categories.GridColor = SystemColors.ActiveBorder;
+            dgv_Categories.Location = new Point(32, 143);
+            dgv_Categories.Name = "dgv_Categories";
+            dgv_Categories.RowHeadersVisible = false;
+            dgv_Categories.RowTemplate.DefaultCellStyle.BackColor = SystemColors.ActiveBorder;
+            dgv_Categories.RowTemplate.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dgv_Categories.Size = new Size(196, 490);
+            dgv_Categories.TabIndex = 0;
+            dgv_Categories.CellClick += dgv_Categories_CellClick;
+            // 
+            // dgv_Tasks
+            // 
+            dgv_Tasks.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv_Tasks.BackgroundColor = SystemColors.ActiveBorder;
+            dgv_Tasks.BorderStyle = BorderStyle.Fixed3D;
+            dgv_Tasks.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dgv_Tasks.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_Tasks.ColumnHeadersVisible = false;
+            dgv_Tasks.GridColor = SystemColors.ActiveBorder;
+            dgv_Tasks.Location = new Point(302, 143);
+            dgv_Tasks.Name = "dgv_Tasks";
+            dgv_Tasks.RowHeadersVisible = false;
+            dgv_Tasks.RowTemplate.DefaultCellStyle.BackColor = SystemColors.ActiveBorder;
+            dgv_Tasks.RowTemplate.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dgv_Tasks.Size = new Size(604, 490);
+            dgv_Tasks.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label1.Location = new Point(32, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 30);
+            label1.TabIndex = 2;
+            label1.Text = "Categories:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            label2.Location = new Point(302, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Category Tasks:";
             // 
             // Form1
             // 
@@ -36,11 +97,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveBorder;
             ClientSize = new Size(946, 698);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgv_Tasks);
+            Controls.Add(dgv_Categories);
             Name = "Form1";
             Text = "TO DO LIST";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgv_Categories).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgv_Tasks).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dgv_Categories;
+        private DataGridView dgv_Tasks;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using DoToListAppWindowsForms.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DoToListAppWindowsForms
 {
     public partial class Form1 : Form
@@ -7,9 +10,35 @@ namespace DoToListAppWindowsForms
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            using (TodolistContext db = new TodolistContext())
+            {
+                var categories = db.Categories.ToList();
+                dgv_Categories.DataSource = categories;
+                var firstCategoryTasks = db.Tasks.Where(t => t.CategoryId == categories.First().CategoryId).ToList();
+                dgv_Tasks.DataSource = firstCategoryTasks;
+            }
 
+            dgv_Categories.Columns["Tasks"].Visible = false;
+            dgv_Categories.Columns["CategoryId"].Visible = false;
+
+            dgv_Tasks.Columns["TaskId"].Visible = false;
+            dgv_Tasks.Columns["CategoryId"].Visible = false;
+            dgv_Tasks.Columns["Category"].Visible = false;
+        }
+
+        private void dgv_Categories_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Category clikedCategory = (Category)dgv_Categories.Rows[e.RowIndex].DataBoundItem;
+
+            using (var db = new TodolistContext())
+            {
+                var tasks = db.Tasks.Where(t => t.CategoryId == clikedCategory.CategoryId).ToList();
+                dgv_Tasks.DataSource = tasks;
+            }
+
+            
         }
     }
 }
